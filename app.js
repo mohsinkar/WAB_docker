a20a51c93ec089748a3bb2da536679abaf5a2978
Mohsin Karim <mohsinkar@users.noreply.github.com>
2018-11-27 12:38:34 -0600

    initial commit

--- app.js
+++ app.js
@@ -1,24 +1,17 @@
 var fs = require('fs');
 var http = require('http');
-var https = require('https');
 var express = require('express');
 var helmet = require('helmet');
 
-var privateKey  = fs.readFileSync('privatekey.key', 'utf8');
-var certificate = fs.readFileSync('certificate.crt', 'utf8');
 
 var app = express();
 app.use(express.static('./'));
 app.use(helmet());
 
-var credentials = {key: privateKey, cert: certificate};
 
 
 // your express configuration here
 
 var httpServer = http.createServer(app);
-var httpsServer = https.createServer(credentials, app);
-
-httpServer.listen(8080);
-httpsServer.listen(3344);
+httpServer.listen(3344);
    
